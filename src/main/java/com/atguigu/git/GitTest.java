3133b5d7e1a9c318e1aa5f3a1cc7419127e2318a
chentong <chentong@inspur.com>
2023-01-12 22:32:14 +0800

    dev commit 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("------");
         System.out.println("------3");
         System.out.println("dev");
+        System.out.println("dev2");
     }
 }
