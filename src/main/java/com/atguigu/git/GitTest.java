b18cda9716197be9e09d4c2ecc8bd85b48ac8c3e
chinact90 <122539154+chinact90@users.noreply.github.com>
2023-01-13 21:47:04 +0800

    pusstest

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("dev");
         System.out.println("dev2");
         System.out.println("push test");
+       System.out.println("pull test");
     }
 }
